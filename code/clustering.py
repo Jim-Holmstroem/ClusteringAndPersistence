3c5bc5b6fadbaca6a91565c8c4940ef0a10a1887
Jim Holmström <jim.holmstroem@gmail.com>
2013-11-10 02:35:29 +0100

    done with the clustering but totally untested

--- code/clustering.py
+++ code/clustering.py
@@ -1,16 +1,45 @@
 from __future__ import print_function, division
 
-from functools import partial
+from itertools import combinations
+from functools import partial, wraps
 from abc import abstractmethod, ABCMeta
 
 import numpy as np
 
-def distance_matrix(distance, a, b):
+def distances(distance, a, b):
     return map(distance, product(a, b))
 
-SingeltonSet = partial(map, lambda x: {x})
+def min_with_arg_by(f, A):
+    min_, min_arg = min(map(lambda a: (f(a), a), A))
+    return min_arg, min_
 
-class Distance(object):
+SingeltonSet = partial(frozenset, partial(map, lambda x: frozenset([x, ])))
+
+filter_non_equal = partial(filter, lambda (x_i, x_j) : x_i == x_j)
+
+def symmetric_lazy(f):  # TODO breakout the lazy parts of the code
+    """See lazy, adds the fact f(i, j) == f(j, i)
+    Will be associated with a SetDistance class,
+    the Distance.distance function is included in the key.
+    Is currently only made to be used with SetDistance.
+    """
+    @wraps(f)
+    def _f(self, i, j):
+        key = (self.distance.__call__.im_func, i, j)
+        key_transpose = (self.distance.__call__.im_func, j, i)
+        if not (key in _f.__cache or key_transpose in _f.__cache):
+            _f.__cache[key] = f(self, i, j)
+
+        value = _f.__cache(key) if key in _f.__cache else _f.__cache(key_transpose)
+
+        return value
+
+    _f.__cache = dict()
+
+    return _f
+
+def symmetric_lazy(f):
+    class Distance(object):
     __metaclass__ = ABCMeta
     @abstractmethod
     def __call__(self, a, b):
@@ -39,13 +68,19 @@ class EuclideanDistance(PointDistance):
 
 class SL(SetDistance):
     def __call__(self, A, B):
-        return np.min(distance_matrix(self.distance, A, B))
+        return np.min(
+            distances(self.distance, A, B)
+        )
 class CL(SetDistance):
     def __call__(self, A, B):
-        return np.max(distance_matrix(self.distance, A, B))
+        return np.max(
+            distances(self.distance, A, B)
+        )
 class AL(SetDistance):
     def __call__(self, A, B):
-        return np.mean(distance_matrix(self.distance, A, B))
+        return np.mean(
+            distances(self.distance, A, B)
+        )
 class HL(SetDistance):
     def __call__(self, A, B):
         raise NotImplementedError('HL not implemented yet, needs housendorf distance')  # TODO does it need another type of `distance_matrix`?!
@@ -59,14 +94,30 @@ class HC(object):
         """
         Parameters
         ----------
-        X : [block : {x : object}]
+        X : {(block : {(x : object)})}
 
         Returns
         -------
-        clustering : [(distance : float, [block : {x : object}])]
+        clustering : [(distance : float, {(block : {(x : object)}})]
         """
+        #  TODO recursive, flatten it with a trampoline
+        assert(len(X) not in (0,))  # TODO handle these cases gracefully and consistently
 
+        if len(X) == 1:
+            return []
+
+        (A, B), min_distance = min_with_arg_by(
+            self.distance,
+            combinations(X, r=2)
+        )
+
+        return [
+            (min_distance, X)  # NOTE if intended to return something else here is the spot
+        ] + (
+            self.__call__((X - (A | B)) | (A | B))
+        )
 
 
 X = SingeltonSet(range(5))
-print(HC(SL)(X))
+for set_distance in [SL, CL, AL]:
+    print(HC(set_distance)(X))
